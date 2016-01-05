5b5100713c133294b43bb0ebdd2a286d1a54a2b9
Saúl Cabrera <saulecabrera@gmail.com>
2016-01-04 20:32:36 -0600

    added documentation for future me

--- before_compile/a.exs
+++ before_compile/a.exs
@@ -1,4 +1,12 @@
 defmodule A do
+  # The following defmacro definition will
+  # run and inject
+  # the specified code at the end of the 
+  # module calling @before_compile A,
+  # before its compilation starts;
+  # in this specific case, the function 
+  # hello/1 will be injected at the end 
+  # module B
   defmacro __before_compile__ (_env) do
     quote do
       def hello, do: "world"
