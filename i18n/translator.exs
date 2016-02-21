e490d8a0c31926f0a24e564343c4e71b2dc48f77
Saúl Cabrera <saulecabrera@gmail.com>
2016-02-21 15:10:24 -0500

    added i18n example

---
+++ i18n/translator.exs
@@ -0,0 +1,61 @@
+defmodule Translator do
+
+  defmacro __using__(_options) do
+    quote do
+      #due to Keyword Lists this: 
+      #Module.register_attribute(__MODULE__, :locales, [accumulate: true, persist: false])
+      #Module.register_attribute(__MODULE__, :locales, [{:accumulate, true}, {:persist, false}])
+      #Module.register_attribute __MODULE__, :locales, [{:accumulate, true}, {:persist, false}]
+      #is equivalent to this:
+      Module.register_attribute __MODULE__, :locales, accumulate: true, persist: false
+      import unquote(__MODULE__), only: [locale: 2]
+      @before_compile unquote(__MODULE__)
+    end
+  end
+
+  defmacro __before_compile__(env) do
+    compile(Module.get_attribute(env.module, :locales))
+  end
+
+  defmacro locale(name, mappings) do
+    quote bind_quoted: [name: name, mappings: mappings] do
+      @locales {name, mappings}
+    end
+  end
+
+  def compile(translations) do
+    translations_ast = for {locale, mappings} <- translations do
+      deftranslations(locale, "", mappings)
+    end
+
+    quote do
+      #body-less fuction
+      #used for: documentation, default parameters, Protocols
+      def t(locale, path, bindings \\ [])
+      unquote(translations_ast)
+      def t(_locale, _path, _bindings), do: {:error, :no_translation}
+    end
+  end
+
+  defp deftranslations(locale, current_path, mappings) do
+    for {key, val} <- do
+      path = append_path(current_path, key)
+      if Keyword.keyword?(val) do
+        deftranslations(locale, path, val)
+      else
+        quote do
+          def t(unquote(locale), unquote(path), bindings) do
+            unquote(interpolate(val))
+          end
+        end
+      end
+    end
+  end
+
+  defp interpolate(string) do
+
+  end
+
+  defp append_path("", next), do: to_string(next)
+  defp append_path(current, next), do: "#{current}.#{next}"
+end
